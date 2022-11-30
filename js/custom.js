e78b855f5d03d45e46e6acb0b9bf1a66f5746ebd
有泉雄太 <arilab.mail@gmail.com>
2022-11-30 10:43:23 +0900

    サーバーデータ

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+//スムーズスクロール
+$(function(){
+	$('a[href^=#]').click(function() {
+		var speed = 1000;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$('body,html').animate({scrollTop:position}, speed, 'easeOutExpo');
+		return false;
+	});
+});
---
+++ js/slick_products.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function(){
+	$(".bxslider").slick({
+		autoplay: true,
+		autoplaySpeed: 2500,
+		speed: 800,
+		dots: true,
+		arrows: true,
+	})
+});	
